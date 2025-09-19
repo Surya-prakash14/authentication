b2302ac30da7732b23ba79d2b56eee811f2599e9
Surya-prakash14 <suryawildlifeooty@gmail.com>
2025-09-19 10:49:05 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+psycopg2://postgres:admin%40123@localhost/auth_db"
+    DATABASE_URL: str = "postgresql+psycopg2://postgres:Surya%40123@1407localhost/auth_db"
 
 settings = Settings()
